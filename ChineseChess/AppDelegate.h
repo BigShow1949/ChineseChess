8d3564fd2bdd6c653f7baa6c64d5685a6f4ae2d6
BigShow1949 <1029883589@qq.com>
2016-11-01 17:32:58 +0800

    Initial Commit

---
+++ ChineseChess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChineseChess
+//
+//  Created by apple on 16/11/1.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChineseChess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChineseChess
+//
+//  Created by apple on 16/11/1.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
